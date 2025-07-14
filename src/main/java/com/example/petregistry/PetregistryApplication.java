55fc230dad25474c42e4196e8cd3446895a23f72
Jonathan Navarro <jonathan.navarro.j@gmail.com>
2025-07-14 17:04:33 -0500

    initial commit

---
+++ src/main/java/com/example/petregistry/PetregistryApplication.java
@@ -0,0 +1,13 @@
+package com.example.petregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetregistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetregistryApplication.class, args);
+    }
+
+}
